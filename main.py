ac8438c3ac193e5bca75dc27b70cdaff9c24a47d
chen-chun-yu <adam@gapp.nthu.edu.tw>
2021-05-23 18:08:30 +0800

    GridSearchCV & RandomizedSearchCV

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import numpy as np
 import matplotlib.pyplot as plt
-
 from itertools import chain
-
 import nltk
+from numpy.lib.function_base import average
+
 import sklearn
 import scipy.stats
 import joblib
+from sklearn.base import BaseEstimator
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RandomizedSearchCV
-
+from sklearn.model_selection import cross_val_score, GridSearchCV
 import sklearn_crfsuite
 from sklearn_crfsuite import scorers
 from sklearn_crfsuite import metrics
 from collections import Counter
-
 import utils
+from time import time
 
 plt.style.use('ggplot')\
 
@@ -29,82 +33,13 @@ def print_transitions(trans_features):
         print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
         
 def utt2features(dialog, i):
-    
-    features = {
-        'a2a':0.,
-        'a2h':0.,
-        'a2n':0.,
-        'a2s':0.,
-
-        'h2a':0.,
-        'h2h':0.,
-        'h2n':0.,
-        'h2s':0.,
-
-        'n2a':0.,
-        'n2h':0.,
-        'n2n':0.,
-        'n2s':0.,
-
-        's2a':0.,
-        's2h':0.,
-        's2n':0.,
-        's2s':0.,
-
-        'Start2a':0.,
-        'Start2h':0.,
-        'Start2n':0.,
-        'Start2s':0.,
-        
-        'pretrained_a':out_dict[dialog[i][0]][0],
-        'pretrained_h':out_dict[dialog[i][0]][1],
-        'pretrained_n':out_dict[dialog[i][0]][2],
-        'pretrained_s':out_dict[dialog[i][0]][3]
-    }
-
     current_ses = dialog[i][0][:5] #Ses01
-    current_spk = dialog[i][1]
-    current_emo = dialog[i][2]
-    previous_emo = ""
-    
-    # find this speaker's previous emotion in this dialog
-    for index in range(i, -1, -1):
-        previous_spk = dialog[index][1]
-        if previous_spk == current_spk:
-            previous_emo = dialog[index][2]
-            break
-    
-    if previous_emo == "":
-        previous_emo = "Start"
-    
-    '''
-    # find the emotion of the previous utt
-    if i-1 >= 0:
-        previous_emo = dialog[i-1][2]
-    else:
-        previous_emo = "Start"
-    '''
-    features[emo_mapping_dict[previous_emo] + '2' + emo_mapping_dict[current_emo]] = intra_emo_trans_prob_dict[current_ses][emo_mapping_dict[previous_emo] + '2' + emo_mapping_dict[current_emo]]
-    
-    '''
-    out_max_index = np.argmax(out_dict[dialog[i][0]])
-    if out_max_index == 0:
-        features['pretrained_a'] = out_dict[dialog[i][0]][0]
-    elif out_max_index == 1:
-        features['pretrained_h'] = out_dict[dialog[i][0]][1]
-    elif out_max_index == 2:
-        features['pretrained_n'] = out_dict[dialog[i][0]][2]
-    elif out_max_index == 3:
-        features['pretrained_s'] = out_dict[dialog[i][0]][3]
-    '''
-    '''
     features = {        
         'pretrained_a':out_dict[dialog[i][0]][0],
         'pretrained_h':out_dict[dialog[i][0]][1],
         'pretrained_n':out_dict[dialog[i][0]][2],
         'pretrained_s':out_dict[dialog[i][0]][3]
     }
-    '''
     return features
 
 def dialog2features(dialog):
@@ -163,13 +98,12 @@ def construct_train_test(emo_dict, dialogs_edit):
     return train_dialogs1, train_dialogs2, train_dialogs3, train_dialogs4, train_dialogs5, test_dialogs1, test_dialogs2, test_dialogs3, test_dialogs4, test_dialogs5
 
 if __name__ == "__main__":
+    start_t = time()
     emo_mapping_dict = {'ang':'a', 'hap':'h', 'neu':'n', 'sad':'s', 'Start':'Start', 'End':'End', 'pre-trained':'p', 0:'ang', 1:'hap', 2:'neu', 3:'sad'}
     emo_dict = joblib.load('data/emo_all_iemocap.pkl')
     dialogs = joblib.load('data/dialog_iemocap.pkl')
     dialogs_edit = joblib.load('data/dialog_4emo_iemocap.pkl')
     out_dict = joblib.load('data/outputs.pkl')
-
-    intra_emo_trans_prob_dict = utils.get_val_emo_trans_prob(emo_dict, dialogs_edit)
 
     train_dialogs1, train_dialogs2, train_dialogs3, train_dialogs4, train_dialogs5, test_dialogs1, test_dialogs2, test_dialogs3, test_dialogs4, test_dialogs5 = construct_train_test(emo_dict, dialogs_edit)
 
@@ -198,37 +132,83 @@ if __name__ == "__main__":
     y5_test = [dialog2labels(s) for s in test_dialogs5]
 
     predict = []
-    
-    crf = sklearn_crfsuite.CRF(algorithm='l2sgd', c2=0.1, max_iterations=100)
-    crf.fit(X1_train, y1_train)
+    '''
+    parameters
+    c2: default = 1.0
+    max_iterations: default=1000
+    period:The duration of iterations to test the stopping criterion. default = 10
+    delta:The threshold for the stopping criterion; an iteration stops when the improvement of the log likelihood over the last period iterations is no greater than this threshold. default = 1e-5
+    calibration_eta:The initial value of learning rate (eta) used for calibration. default=0.1
+    calibration_rate:The rate of increase/decrease of learning rate for calibration. default=2.0
+    calibration_samples:The number of instances used for calibration. The calibration routine randomly chooses instances no larger than calibration_samples. default=1000
+    calibration_candidates:The number of candidates of learning rate. The calibration routine terminates after finding calibration_samples candidates of learning rates that can increase log-likelihood. default=10
+    calibration_max_trials:The maximum number of trials of learning rates for calibration. The calibration routine terminates after trying calibration_max_trials candidate values of learning rates. default=20
+    '''
+    crf = sklearn_crfsuite.CRF(algorithm='l2sgd') 
+    params_space = {
+        'c2': np.linspace(start = 0.01, stop = 1,  endpoint=True, num = 10),
+        'max_iterations': range(800, 1201, 100),
+        'period': range(8, 13, 1),
+        'delta': np.linspace(start = 0.000001, stop = 0.0001,  endpoint = True, num = 20),
+        'calibration_eta': np.linspace(start = 0.001, stop = 0.1,  endpoint = True, num = 20),
+        'calibration_rate': np.linspace(start = 1.0, stop = 5.0,  endpoint = True, num = 5),
+        'calibration_samples': range(1000, 1501, 250),
+        'calibration_candidates': [5, 10 , 15],
+        'calibration_max_trials': [15, 20, 25]
+    }
+    # use the same metric for evaluation
+    scorer = make_scorer(metrics.flat_accuracy_score)
+    #s_CV = RandomizedSearchCV(crf, params_space, cv=5, verbose=1, n_jobs=-1, n_iter=200, scoring=recall_scorer, random_state=0)
+    s_CV = GridSearchCV(crf, params_space, cv=5, verbose=1, n_jobs=-1, scoring=scorer)
+    s_CV.fit(X1_train, y1_train)
+    crf = s_CV.best_estimator_
     y1_pred = crf.predict(X1_test)
     for sub_list in y1_pred:
         predict += sub_list
     joblib.dump(crf, 'model/Ses01.sav')
-
-    crf.fit(X2_train, y2_train)
+    end_t = time()
+    dur = (end_t-start_t)/60.
+    print('Process time(1):', dur, 'min')
+    
+    s_CV.fit(X2_train, y2_train)
+    crf = s_CV.best_estimator_
     y2_pred = crf.predict(X2_test)
     for sub_list in y2_pred:
         predict += sub_list
     joblib.dump(crf, 'model/Ses02.sav')
+    end_t = time()
+    dur = (end_t-start_t)/60.
+    print('Process time(2):', dur, 'min')
 
-    crf.fit(X3_train, y3_train)
+    s_CV.fit(X3_train, y3_train)
+    crf = s_CV.best_estimator_
     y3_pred = crf.predict(X3_test)
     for sub_list in y3_pred:
         predict += sub_list
     joblib.dump(crf, 'model/Ses03.sav')
+    end_t = time()
+    dur = (end_t-start_t)/60.
+    print('Process time(3):', dur, 'min')
 
-    crf.fit(X4_train, y4_train)
+    s_CV.fit(X4_train, y4_train)
+    crf = s_CV.best_estimator_
     y4_pred = crf.predict(X4_test)
     for sub_list in y4_pred:
         predict += sub_list
     joblib.dump(crf, 'model/Ses04.sav')
+    end_t = time()
+    dur = (end_t-start_t)/60.
+    print('Process time(4):', dur, 'min')
 
-    crf.fit(X5_train, y5_train)
+    s_CV.fit(X5_train, y5_train)
+    crf = s_CV.best_estimator_
     y5_pred = crf.predict(X5_test)
     for sub_list in y5_pred:
         predict += sub_list
     joblib.dump(crf, 'model/Ses05.sav')
+    end_t = time()
+    dur = (end_t-start_t)/60.
+    print('Process time(5):', dur, 'min')
 
     label = []
     for _, dia in enumerate(dialogs):
@@ -243,15 +223,28 @@ if __name__ == "__main__":
             predict[i] = 2
         elif predict[i] == 'sad':
             predict[i] = 3
-
+    
+    print('Best score for training data:', s_CV.best_score_)
+    print('Best c2:', s_CV.best_estimator_.c2)
+    print('Best max_iterations', s_CV.best_estimator_.max_iterations)
+    print('Best period:', s_CV.best_estimator_.period) 
+    print('Best delta:', s_CV.best_estimator_.delta)
+    print('Best calibration_eta', s_CV.best_estimator_.calibration_eta)
+    print('Best calibration_rate', s_CV.best_estimator_.calibration_rate)
+    print('Best calibration_samples', s_CV.best_estimator_.calibration_samples)
+    print('Best calibration_candidates', s_CV.best_estimator_.calibration_candidates)
+    print('Best calibration_max_trials', s_CV.best_estimator_.calibration_max_trials)
+    
     uar, acc, conf = utils.evaluate(predict, label)
     print('UAR:', uar)
     print('ACC:', acc)
     print(conf)
+    '''
     print('===================================================') 
     print("Top likely transitions:") # transition feature coefficients {(label_from, label_to) -- coef}
     print_transitions(Counter(crf.transition_features_).most_common(len(crf.transition_features_)))
     print('===================================================')
     print("Top positive:") # state feature coefficients {(attr_name, label) -- coef}
     print_state_features(Counter(crf.state_features_).most_common(len(crf.state_features_)))
+    '''
     
--- utils.py
+++ utils.py
@@ -40,6 +40,7 @@ def split_dialog(dialogs):
         spk_dialogs[dialog_id+'_F'].append(utt_id)
 
   return spk_dialogs
+
 def emo_trans_prob_BI_without_softmax_intra(emo_dict, dialogs, val=None):
     # only estimate anger, happiness, neutral, sadness
     Start2a = 0
@@ -183,15 +184,162 @@ def emo_trans_prob_BI_without_softmax_intra(emo_dict, dialogs, val=None):
             'Start2a':Start2a/Start2, 'Start2h':Start2h/Start2, 'Start2n':Start2n/Start2, 'Start2s':Start2s/Start2, \
             'a2End':a2End/End2, 'h2End':h2End/End2, 'n2End':n2End/End2, 's2End':s2End/End2 }
 
+def emo_trans_prob_BI_without_softmax_seq(emo_dict, dialogs, val=None):
+    # only estimate anger, happiness, neutral, sadness
+    Start2a = 0
+    Start2h = 0
+    Start2n = 0
+    Start2s = 0
+    
+    a2End = 0
+    h2End = 0
+    n2End = 0
+    s2End = 0
+    
+    Start2 = 0
+    End2 = 0
+    a2 = 0
+    h2 = 0
+    n2 = 0
+    s2 = 0
+
+    ang2ang = 0
+    ang2hap = 0
+    ang2neu = 0
+    ang2sad = 0
+    
+    hap2ang = 0
+    hap2hap = 0
+    hap2neu = 0
+    hap2sad = 0
+
+    neu2ang = 0
+    neu2hap = 0
+    neu2neu = 0
+    neu2sad = 0
+
+    sad2ang = 0
+    sad2hap = 0
+    sad2neu = 0
+    sad2sad = 0
+
+    for dialog in dialogs.values():
+        utt_num = 1
+        pre_emo = ''
+        for utt in dialog:
+            dialog_id = utt[0:-5]
+            if val and val == dialog_id[0:5]:
+                continue
+            if utt_num == 1:
+                Start2 += 1
+                if emo_dict[utt] == 'ang':
+                    Start2a += 1
+                elif emo_dict[utt] == 'hap':
+                    Start2h += 1
+                elif emo_dict[utt] == 'neu':
+                    Start2n += 1
+                elif emo_dict[utt] == 'sad':
+                    Start2s += 1
+
+            if utt_num == len(dialog):
+                End2 += 1
+                if emo_dict[utt] == 'ang':
+                    a2End += 1
+                elif emo_dict[utt] == 'hap':
+                    h2End += 1
+                elif emo_dict[utt] == 'neu':
+                    n2End += 1
+                elif emo_dict[utt] == 'sad':
+                    s2End += 1
+            utt_num += 1
+
+            if pre_emo == '': # begining of the traversal
+                pre_emo = emo_dict[utt]
+                continue
+
+            if pre_emo == 'ang' and emo_dict[utt] == 'ang':
+                ang2ang += 1
+                a2 += 1
+            if pre_emo == 'ang' and emo_dict[utt] == 'hap':
+                ang2hap += 1
+                a2 += 1
+            if pre_emo == 'ang' and emo_dict[utt] == 'neu':
+                ang2neu += 1
+                a2 += 1
+            if pre_emo == 'ang' and emo_dict[utt] == 'sad':
+                ang2sad += 1
+                a2 += 1
+
+            if pre_emo == 'hap' and emo_dict[utt] == 'ang':
+                hap2ang += 1
+                h2 += 1
+            if pre_emo == 'hap' and emo_dict[utt] == 'hap':
+                hap2hap += 1
+                h2 += 1
+            if pre_emo == 'hap' and emo_dict[utt] == 'neu':
+                hap2neu += 1
+                h2 += 1
+            if pre_emo == 'hap' and emo_dict[utt] == 'sad':
+                hap2sad += 1
+                h2 += 1
+
+            if pre_emo == 'neu' and emo_dict[utt] == 'ang':
+                neu2ang += 1
+                n2 += 1
+            if pre_emo == 'neu' and emo_dict[utt] == 'hap':
+                neu2hap += 1
+                n2 += 1
+            if pre_emo == 'neu' and emo_dict[utt] == 'neu':
+                neu2neu += 1
+                n2 += 1
+            if pre_emo == 'neu' and emo_dict[utt] == 'sad':
+                neu2sad += 1
+                n2 += 1
+
+            if pre_emo == 'sad' and emo_dict[utt] == 'ang':
+                sad2ang += 1
+                s2 += 1
+            if pre_emo == 'sad' and emo_dict[utt] == 'hap':
+                sad2hap += 1
+                s2 += 1
+            if pre_emo == 'sad' and emo_dict[utt] == 'neu':
+                sad2neu += 1
+                s2 += 1
+            if pre_emo == 'sad' and emo_dict[utt] == 'sad':
+                sad2sad += 1
+                s2 += 1
+
+            pre_emo = emo_dict[utt]
+    '''
+    print(ang2ang/a2+ang2hap/a2+ang2neu/a2+ang2sad/a2)
+    print(hap2ang/h2+hap2hap/h2+hap2neu/h2+hap2sad/h2)
+    print(neu2ang/n2+neu2hap/n2+neu2neu/n2+neu2sad/n2)
+    print(sad2ang/s2+sad2hap/s2+sad2neu/s2+sad2sad/s2)
+    #print((Start2a+Start2h+Start2n+Start2s)/Start2)
+    #print((a2End+h2End+n2End+s2End)/End2)
+    '''
+    #print('=============================================')
+    
+    return {'a2a':ang2ang/a2, 'a2h':ang2hap/a2, 'a2n':ang2neu/a2, 'a2s':ang2sad/a2, \
+            'h2a':hap2ang/h2, 'h2h':hap2hap/h2, 'h2n':hap2neu/h2, 'h2s':hap2sad/h2, \
+            'n2a':neu2ang/n2, 'n2h':neu2hap/n2, 'n2n':neu2neu/n2, 'n2s':neu2sad/n2, \
+            's2a':sad2ang/s2, 's2h':sad2hap/s2, 's2n':sad2neu/s2, 's2s':sad2sad/s2, \
+            'Start2a':Start2a/Start2, 'Start2h':Start2h/Start2, 'Start2n':Start2n/Start2, 'Start2s':Start2s/Start2, \
+            'a2End':a2End/End2, 'h2End':h2End/End2, 'n2End':n2End/End2, 's2End':s2End/End2 }
+
 def get_val_emo_trans_prob(emo_dict, dialogs):
     """Get emo_trans_prob estimated from training sessions."""
 
     session = ['Ses01', 'Ses02', 'Ses03', 'Ses04', 'Ses05']
     intra_emo_trans_prob_dict = {}
+    seq_emo_trans_prob_dict = {}
     for i in range(len(session)):
         val = session[i]
 
         intra_emo_trans_prob_com = emo_trans_prob_BI_without_softmax_intra(emo_dict, dialogs, val)
         intra_emo_trans_prob_dict[val] = intra_emo_trans_prob_com
 
-    return intra_emo_trans_prob_dict
+        seq_emo_trans_prob_com = emo_trans_prob_BI_without_softmax_seq(emo_dict, dialogs, val)
+        seq_emo_trans_prob_dict[val] = seq_emo_trans_prob_com
+    
+    return intra_emo_trans_prob_dict, seq_emo_trans_prob_dict
